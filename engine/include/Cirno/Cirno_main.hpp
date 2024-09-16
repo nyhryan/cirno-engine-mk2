95ea6b0763ebb99a46162a9dda7fba3658fa0614
ATAI <nyhryan99102@gmail.com>
2024-09-16 18:58:15 +0900

    dump2

--- engine/include/Cirno/Cirno_main.hpp
+++ engine/include/Cirno/Cirno_main.hpp
@@ -1,9 +1,12 @@
 #include "Cirno/Application.hpp"
+#include "Cirno/Logger.hpp"
+#include "spdlog/common.h"
 
 extern Cirno::Application *Cirno::CreateApplication();
 
 int main(int, char **)
 {
+
     auto *app = Cirno::CreateApplication();
     app->Run();
     delete app;
